627a92e4726eae6a79f6c7a4f2395f2b246c2a3d
Ola Petersson <ola.petersson@squeed.com>
2018-02-14 21:23:57 +0100

    Ajabaja

--- app/src/main/java/com/example/ola/myapplication/AddBookActivity.kt
+++ app/src/main/java/com/example/ola/myapplication/AddBookActivity.kt
@@ -3,29 +3,43 @@ package com.example.ola.myapplication
 import android.os.Bundle
 import android.support.design.widget.TextInputEditText
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
 import android.widget.NumberPicker
+import android.widget.Toast
 
 class AddBookActivity : AppCompatActivity() {
-    private lateinit var numberPicker: NumberPicker
-    private lateinit var textInputEditText: TextInputEditText
+    private lateinit var bookScorePicker: NumberPicker
+    private lateinit var bookTitleInput: TextInputEditText
+    private lateinit var submitBookButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_book)
         setupNumberPicker()
         setupTextInput()
+        setupButton()
+    }
+
+    private fun setupButton(){
+        submitBookButton = findViewById<Button>(R.id.btnSubmitBook) as Button
+        submitBookButton.setOnClickListener{ view ->
+            if(bookTitleInput.text.isBlank()){
+                Toast.makeText(this , "Ajabaja", Toast.LENGTH_SHORT).show()
+            }
+            Book(bookTitleInput.text.toString(), bookScorePicker.value)
+        }
     }
 
     private fun setupTextInput() {
-        textInputEditText = findViewById<TextInputEditText>(R.id.txtInBookTitle) as TextInputEditText
+        bookTitleInput = findViewById<TextInputEditText>(R.id.txtInBookTitle) as TextInputEditText
 
     }
 
     private fun setupNumberPicker() {
-        numberPicker = findViewById<NumberPicker>(R.id.npBookScore) as NumberPicker
-        numberPicker.minValue = 1
-        numberPicker.maxValue = 4
-        numberPicker.displayedValues = arrayOf("Bad", "OK", "Good", "Very Good")
+        bookScorePicker = findViewById<NumberPicker>(R.id.npBookScore) as NumberPicker
+        bookScorePicker.minValue = 1
+        bookScorePicker.maxValue = 4
+        bookScorePicker.displayedValues = arrayOf("Bad", "OK", "Good", "Very Good")
     }
 
 }
---
+++ app/src/main/java/com/example/ola/myapplication/Book.kt
@@ -0,0 +1,6 @@
+package com.example.ola.myapplication
+
+/**
+ * Created by Ola on 2018-02-14.
+ */
+data class Book(val title: String, val score: Number)
