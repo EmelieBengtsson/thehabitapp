6763b57cb9443f390f8bcf6d1ceb6a8e188141b2
Ola Petersson <ola.petersson@squeed.com>
2018-02-24 15:28:40 +0100

    lista

--- app/src/main/java/com/example/ola/myapplication/AddBookActivity.kt
+++ app/src/main/java/com/example/ola/myapplication/AddBookActivity.kt
@@ -1,19 +1,27 @@
 package com.example.ola.myapplication
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.support.design.widget.TextInputEditText
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.widget.Button
 import android.widget.NumberPicker
 import android.widget.Toast
+import com.google.gson.Gson
 
 class AddBookActivity : AppCompatActivity() {
+    val SHARED_PREF_KEY="habitappen"
+    val SHARED_PREF_BOOKS="books"
+
     private lateinit var bookScorePicker: NumberPicker
     private lateinit var bookTitleInput: TextInputEditText
     private lateinit var submitBookButton: Button
+    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        sharedPreferences = this.getSharedPreferences(SHARED_PREF_KEY, android.content.Context.MODE_PRIVATE)
         setContentView(R.layout.activity_add_book)
         setupNumberPicker()
         setupTextInput()
@@ -25,13 +33,24 @@ class AddBookActivity : AppCompatActivity() {
         submitBookButton.setOnClickListener{ view ->
             if(bookTitleInput.text.isBlank()){
                 Toast.makeText(this , "Ajabaja", Toast.LENGTH_SHORT).show()
+            } else {
+                val bookToBeAdded = Book(bookTitleInput.text.toString(), bookScorePicker.value)
+                val gson = Gson()
+                val jsonBook = gson.toJson(bookToBeAdded)
+                val editor = sharedPreferences.edit()
+                editor.putString(bookToBeAdded.title, jsonBook)
+                editor.apply()
+                Toast.makeText(this , "Saved ${bookToBeAdded.title} with score ${bookToBeAdded.score}", Toast.LENGTH_LONG).show()
+                setupTextInput()
+                setupNumberPicker()
             }
-            Book(bookTitleInput.text.toString(), bookScorePicker.value)
+
         }
     }
 
     private fun setupTextInput() {
         bookTitleInput = findViewById<TextInputEditText>(R.id.txtInBookTitle) as TextInputEditText
+        bookTitleInput.setText("")
 
     }
 
--- app/src/main/java/com/example/ola/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/ola/myapplication/MainActivity.kt
@@ -5,19 +5,45 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.ListView
+import android.content.SharedPreferences
+import android.widget.ArrayAdapter
+import android.widget.ListAdapter
+import com.google.gson.Gson
 
 class MainActivity : AppCompatActivity() {
+    val SHARED_PREF_KEY="habitappen"
+    private lateinit var listFinishedBooks: ListView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val createBookButton = findViewById<Button>(R.id.btnCreateBook)
-        createBookButton.setOnClickListener { view ->
+        val addBookButton = findViewById<Button>(R.id.btnAddBook)
+        addBookButton.setOnClickListener { view ->
             Log.i("Emelie","somebody clicked the button")
             val addBookIntent = Intent(this, AddBookActivity::class.java)
             startActivity(addBookIntent)
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        setupListFinishedBooks()
+    }
+
+    private fun setupListFinishedBooks() {
+        listFinishedBooks = findViewById<ListView>(R.id.listFinishedBooks)
+        val sharedPreferences = this.getSharedPreferences(SHARED_PREF_KEY, android.content.Context.MODE_PRIVATE)
+        val bookJsons  = sharedPreferences.all.values.toList() as List<String>
+
+         val presentationBooks = bookJsons.map{bookJson: String ->
+            val book=Gson().fromJson(bookJson, Book::class.java)
+             "${book.title} : ${book.score}"
+        }.toList()
+
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, presentationBooks)
+        listFinishedBooks.adapter =adapter
     }
 
 
